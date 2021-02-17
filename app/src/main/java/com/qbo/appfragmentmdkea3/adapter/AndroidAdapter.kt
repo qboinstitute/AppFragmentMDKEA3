8a6c071cf8daec8e43e808bc33a944cff137b924
LuisAngel <luisangel.salvat@gmail.com>
2021-02-16 20:51:50 -0500

    tercera parte (160221)

--- app/src/main/java/com/qbo/appfragmentmdkea3/adapter/AndroidAdapter.kt
+++ app/src/main/java/com/qbo/appfragmentmdkea3/adapter/AndroidAdapter.kt
@@ -1,26 +1,35 @@
 package com.qbo.appfragmentmdkea3.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.qbo.appfragmentmdkea3.R
 import com.qbo.appfragmentmdkea3.model.Android
 
-class AndroidAdapter(private var lstandroid: List<Android>,
-                        private var context: Context)
+class AndroidAdapter(private var lstandroid: List<Android>)
     : RecyclerView.Adapter<AndroidAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        val ivandroid: ImageView = itemView.findViewById(R.id.ivandroid)
+        val tvnomandroid: TextView = itemView.findViewById(R.id.tvnomandroid)
+        val tvversionandroid: TextView = itemView.findViewById(R.id.tvversionandroid)
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AndroidAdapter.ViewHolder {
-        TODO("Not yet implemented")
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return ViewHolder(layoutInflater.inflate(R.layout.item_android, parent, false))
     }
     override fun onBindViewHolder(holder: AndroidAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val itemAndroid = lstandroid[position]
+        holder.tvnomandroid.text = itemAndroid.nombre
+        holder.tvversionandroid.text = itemAndroid.version
+        holder.ivandroid.setImageResource(itemAndroid.imagen)
     }
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return lstandroid.size
     }
 
 }
