6e47b1b28cae9a23fc28d9f23240f500420f5af8
LuisAngel <luisangel.salvat@gmail.com>
2021-02-16 22:46:12 -0500

    cuarta  parte (160221)

--- app/src/main/java/com/qbo/appfragmentmdkea3/ui/InicioFragment.kt
+++ app/src/main/java/com/qbo/appfragmentmdkea3/ui/InicioFragment.kt
@@ -5,19 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.snackbar.Snackbar
 import com.qbo.appfragmentmdkea3.R
+import com.qbo.appfragmentmdkea3.databinding.FragmentInicioBinding
 
 
 class InicioFragment : Fragment() {
 
+    private var bindingini : FragmentInicioBinding? = null
+    private val binding get() = bindingini!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicio, container, false)
+        bindingini = FragmentInicioBinding.inflate(inflater, container, false)
+        val vista = binding.root
+        binding.btncalcular.setOnClickListener {
+            val nota1 = binding.etnota1.text.toString().toInt()
+            val nota2 = binding.etnota2.text.toString().toInt()
+            val promedio = (nota1 + nota2) / 2
+            Snackbar.make(it, "Promedio $promedio", Snackbar.LENGTH_LONG).show()
+        }
+        return vista
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        bindingini = null
+    }
 
 }
