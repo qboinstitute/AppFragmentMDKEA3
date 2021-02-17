0a6479b6fd5e4625eaad484c34587e85c390eace
LuisAngel <luisangel.salvat@gmail.com>
2021-02-16 20:06:33 -0500

    segunda parte (160221)

---
+++ app/src/main/java/com/qbo/appfragmentmdkea3/adapter/AndroidAdapter.kt
@@ -0,0 +1,26 @@
+package com.qbo.appfragmentmdkea3.adapter
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.qbo.appfragmentmdkea3.model.Android
+
+class AndroidAdapter(private var lstandroid: List<Android>,
+                        private var context: Context)
+    : RecyclerView.Adapter<AndroidAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AndroidAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+    override fun onBindViewHolder(holder: AndroidAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ app/src/main/java/com/qbo/appfragmentmdkea3/model/Android.kt
@@ -0,0 +1,7 @@
+package com.qbo.appfragmentmdkea3.model
+
+data class Android(val id: Int,
+                   val imagen: Int,
+                   val nombre: String,
+                   val version: String
+)
